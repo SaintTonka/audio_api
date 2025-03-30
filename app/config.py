989999e64c2aaedfbf5694010f44754af479d454
Daniil Gavrilov <daniilgavrilov9994@gmail.com>
2025-03-30 13:18:18 +0300

    Fix config

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
 
 class Settings(BaseSettings):
     POSTGRES_USER: str
@@ -14,8 +15,12 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    @property
+    def database_url(self):
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
     
     class Config:
-        env_file = ".env"
+        env_file = Path(__file__).parent.parent / ".env"
 
 settings = Settings()
